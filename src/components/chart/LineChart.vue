f83e886f33886ef3d3d5401c29005e7cf1ae8e1c
hui <244701374@qq.com>
2020-01-17 18:22:58 +0800

    echart图调整

--- src/components/chart/LineChart.vue
+++ src/components/chart/LineChart.vue
@@ -35,105 +35,108 @@ export default class LineChart extends Vue {
       title: {
           text: '未来一周气温变化',
           textStyle:{
-              color:'#fff',
-              fontStyle:'normal',
-              fontWeight:'normal',
-              fontSize:'16',
+              color: '#fff',
+              fontStyle: 'normal',
+              fontWeight: 'normal',
+              fontSize: '16',
               borderWidth: '10',
           },
          left: '100',
         },
         tooltip: {
-            trigger: 'axis'
+            trigger: 'axis',
         },
         legend: {
-            right:'10%',
+            right: '10%',
             data: [
                 {
                     name: '最高气温',
-                    icon: 'circle'
+                    icon: 'circle',
                 },
                 {
                     name: '最低气温',
-                    icon: 'circle'
-                }
-            ]
+                    icon: 'circle',
+                },
+            ],
         },
         toolbox: {
             show: true,
             feature: {
                 magicType: {type: ['line', 'bar']},
-            }
+            },
         },
         xAxis:  {
             type: 'category',
             boundaryGap: false,
-            data: ['周一','周二','周三','周四','周五','周六','周日']
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
         },
         yAxis: {
             type: 'value',
             axisLabel: {
-                formatter: '{value} °C'
-            }
+                formatter: '{value} °C',
+            },
         },
         series: [
             {
-                name:'最高气温',
-                type:'line',
-                color: "#FC9DB2", //line tooltip color
-                lineStyle: { //line color
+                name: '最高气温',
+                type: 'line',
+                color: "#FC9DB2", // line tooltip color
+                lineStyle: { // line color
                     color: "#FA5071"
                 },
                 smooth: true,
-                data:[11, 11, 15, 13, 12, 13, 10],
+                data: [11, 11, 15, 13, 12, 13, 10],
                 markPoint: {
                     data: [
                         {type: 'max', name: '最大值'},
                         {type: 'min', name: '最小值'}
-                    ]
+                    ],
                 },
                 markLine: {
                     data: [
                         {type: 'average', name: '平均值'}
-                    ]
-                }
+                    ],
+                },
             },
             {
-                name:'最低气温',
-                type:'line',
-                color: "#9DC4FA", //折线图颜色,搭配markArea为面积图
-                lineStyle: { //折线的颜色
+                name: '最低气温',
+                type: 'line',
+                color: "#9DC4FA", // 折线图颜色,搭配markArea为面积图
+                lineStyle: { // 折线的颜色
                     color: "#3B9DFC"
                 },
-                smooth: true, //平滑处理
-                data:[1, -2, 2, 5, 3, 2, 0],
+                smooth: true, // 平滑处理
+                data: [1, -2, 2, 5, 3, 2, 0],
                 markPoint: {
                     data: [
-                        {name: '周最低', value: -2, xAxis: 1, yAxis: -1.5}
-                    ]
+                        {name: '周最低', value: -2, xAxis: 1, yAxis: -1.5},
+                    ],
                 },
                 markLine: {
                     data: [
                         {type: 'average', name: '平均值'},
-                        [{
-                            symbol: 'none',
-                            x: '90%',
-                            yAxis: 'max'
-                        }, {
-                            symbol: 'circle',
-                            label: {
-                                normal: {
-                                    position: 'start',
-                                    formatter: '最大值'
-                                }
+                        [
+                            {
+                                symbol: 'none',
+                                x: '90%',
+                                yAxis: 'max',
                             },
-                            type: 'max',
-                            name: '最高点'
-                        }]
-                    ]
-                }
-            }
-        ]
+                            {
+                                symbol: 'circle',
+                                label: {
+                                    normal: {
+                                        position: 'start',
+                                        formatter: '最大值',
+                                    },
+                                },
+                                type: 'max',
+                                name: '最高点',
+                            },
+                        ],
+                    ],
+                },
+            },
+        ],
     };
 
   // smallLineChart
@@ -145,8 +148,8 @@ export default class LineChart extends Vue {
         height: '70px',
         width: '100%',
       },
-      xAxis: {show :false},
-      yAxis: {show :false},
+      xAxis: {show: false},
+      yAxis: {show: false},
       grid: {
           top: '0',
           bottom: '2%',
@@ -157,10 +160,10 @@ export default class LineChart extends Vue {
         '零食丰富','每周羽毛球','周末双休','零食丰富','每周羽毛球'],
       seriesData: [0,80, 40, 178, 40,123,170, 40,123,0],
       series: {
-          showSymbol:false,
+          showSymbol: false,
           areaStyle: {
-              color:'rgb(137,75,222)'  //线条下面积颜色
-          }
+              color: 'rgb(137,75,222)',  // 线条下面积颜色
+          },
       },
   };
 
--- src/components/chart/PieChart.vue
+++ src/components/chart/PieChart.vue
@@ -30,16 +30,17 @@ export default class PieChart extends Vue {
     this.chart.setOption({
         title : {
             text: '南丁格尔玫瑰图',
-            x:'center'
+            x: 'center',
         },
         tooltip : {
             trigger: 'item',
-            formatter: "{a} <br/>{b} : {c} ({d}%)"
+            formatter: '{a} <br/>{b} : {c} ({d}%)',
         },
         legend: {
             x : 'center',
             y : 'bottom',
-            data:['rose1','rose2','rose3','rose4','rose5','rose6','rose7','rose8']
+            data:['rose1', 'rose2', 'rose3', 'rose4', 'rose5',
+              'rose6', 'rose7', 'rose8'],
         },
         toolbox: {
             show : true,
@@ -48,65 +49,65 @@ export default class PieChart extends Vue {
                 dataView : {show: true, readOnly: false},
                 magicType : {
                     show: true,
-                    type: ['pie', 'funnel']
+                    type: ['pie', 'funnel'],
                 },
                 restore : {show: true},
-                saveAsImage : {show: true}
-            }
+                saveAsImage : {show: true},
+            },
         },
         calculable : true,
         series : [
             {
-                name:'半径模式',
-                type:'pie',
+                name: '半径模式',
+                type: 'pie',
                 radius : [20, 110],
                 center : ['25%', '50%'],
-                roseType : 'radius',
+                roseType: 'radius',
                 label: {
                     normal: {
-                        show: false
+                        show: false,
                     },
                     emphasis: {
-                        show: true
-                    }
+                        show: true,
+                    },
                 },
-                lableLine: {
+                labelLine: {
                     normal: {
-                        show: false
+                        show: false,
                     },
                     emphasis: {
-                        show: true
-                    }
+                        show: true,
+                    },
                 },
                 data:[
-                    {value:10, name:'rose1'},
-                    {value:5, name:'rose2'},
-                    {value:15, name:'rose3'},
-                    {value:25, name:'rose4'},
-                    {value:20, name:'rose5'},
-                    {value:35, name:'rose6'},
-                    {value:30, name:'rose7'},
-                    {value:40, name:'rose8'}
-                ]
+                    {value: 10, name: 'rose1'},
+                    {value: 5, name: 'rose2'},
+                    {value: 15, name: 'rose3'},
+                    {value: 25, name: 'rose4'},
+                    {value: 20, name: 'rose5'},
+                    {value: 35, name: 'rose6'},
+                    {value: 30, name: 'rose7'},
+                    {value: 40, name: 'rose8'}
+                ],
             },
             {
-                name:'面积模式',
-                type:'pie',
-                radius : [30, 110],
-                center : ['75%', '50%'],
-                roseType : 'area',
+                name: '面积模式',
+                type: 'pie',
+                radius: [30, 110],
+                center: ['75%', '50%'],
+                roseType: 'area',
                 data:[
-                    {value:10, name:'rose1'},
-                    {value:5, name:'rose2'},
-                    {value:15, name:'rose3'},
-                    {value:25, name:'rose4'},
-                    {value:20, name:'rose5'},
-                    {value:35, name:'rose6'},
-                    {value:30, name:'rose7'},
-                    {value:40, name:'rose8'}
-                ]
-            }
-        ]
+                    {value: 10, name: 'rose1'},
+                    {value: 5, name: 'rose2'},
+                    {value: 15, name: 'rose3'},
+                    {value: 25, name: 'rose4'},
+                    {value: 20, name: 'rose5'},
+                    {value: 35, name: 'rose6'},
+                    {value: 30, name: 'rose7'},
+                    {value: 40, name: 'rose8'}
+                ],
+            },
+        ],
     });
   }
 
