c1afe22febdf6a6103e8a4d078387a0669789316
hui <244701374@qq.com>
2021-04-27 20:56:12 +0800

    ..

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
   <div id="app">
-    <Index />
+    <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import Index from '@/views/Index.vue';
-@Component({
-  components: {
-    Index,
-  },
-})
-export default class App extends Vue {}
+<script>
+
+export default {
+  name: 'app',
+}
 </script>
 
-<style lang="scss">
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
