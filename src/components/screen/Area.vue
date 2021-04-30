f7afede06f7d77a22182a43dccc2941ef2376de0
刘慧慧 <244701374@qq.com>
2021-04-30 18:30:27 +0800

    '.'

---
+++ src/components/screen/Area.vue
@@ -0,0 +1,35 @@
+<template>
+    <div :style="{
+        left: start.x + 'px',
+        top: start.y + 'px',
+        width: width + 'px',
+        height: height + 'px',
+    }" class="area"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Emit } from 'vue-property-decorator';
+import Grid from './Grid.vue'
+import Area from './Area.vue'
+/**
+ * 画布-center
+ */
+@Component({
+  name: 'screenArea'
+})
+export default class ScreenArea extends Vue {
+  @Prop({default: {x: 0, y: 0}}) private start!: {
+    x: number,
+    y: number
+  };
+  @Prop({default: 100}) private width!: number;
+  @Prop({default: 100}) private height!: number;
+}
+</script>
+
+<style lang="scss" scoped>
+.area {
+    border: 1px solid #70c0ff;
+    position: absolute;
+}
+</style>
---
+++ src/components/screen/TabAttr.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="screen-con-r">
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <template v-for="(tab, index) in tabList" >
+        <el-tab-pane :label="tab.label" :name="tab.name" :key="index">
+          <!-- <ListIndex :name="tab.component" /> -->
+          11111111
+        </el-tab-pane>
+      </template>
+    </el-tabs>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+/**
+ * 画布-center
+ */
+@Component({name: 'tabAttr'})
+export default class TabAttr extends Vue {
+  private tabList: Array<{label: string, name: string, component: string }> = [
+    {
+      label: '属性',
+      name: 'attri',
+      component: 'default'
+    },
+    {
+      label: '动画',
+      name: 'anim',
+      component: 'element'
+    },
+    {
+      label: '事件',
+      name: 'events',
+      component: 'element'
+    }
+  ];
+  // 右侧栏，选中tab
+  private activeName: string = 'attri';
+
+  // tab 切换事件
+  private handleClick(tab: string, event: any) {
+    console.log(tab);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
